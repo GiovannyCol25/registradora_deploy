c5545d0474b337d367a5bbabff897052f4173268
GiovannySoft <capera.giovanny25@gmail.com>
2025-11-21 19:40:48 -0500

    modificaciones de prueba para inicio de proyecto

---
+++ src/main/resources/db.migration/V15__insert_empleado_admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO empleados (nombre_empleado, cargo, telefono)
+VALUES ('Maricela Zambrano', 'Administrador', 3120001111);
---
+++ src/main/resources/db.migration/V16__insert_usuario_admin.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuarios (login, clave, rol)
+VALUES (
+    'MaryZamVend',
+    '$2a$12$VnmGuzRqD4NhfuRyYo.fluGZEefMIG2dWOqilyW6kydvVTvm99uaK',
+    'ADMIN',
+);
